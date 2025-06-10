da037d1bf10a138153ca43bf23793725afad579e
jcurbelo <robin@paella.dev>
2025-06-10 11:32:12 -0400

    encapsulated footer as a component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import Image from "next/image";
 import { useState } from "react";
 import { Button } from "../components/button";
 import { ItemsCard } from "../components/items-card";
 import { CheckoutDialog } from "../components/checkout-dialog";
 import { WEAPONS } from "@/lib/api";
+import { Footer } from "@/components/footer";
 
 export default function Home() {
   const [selectedWeapon, setSelectedWeapon] = useState("gods-sword");
@@ -41,70 +41,12 @@ export default function Home() {
           </div>
         </div>
       </main>
-      <footer className="flex flex-col gap-4 items-center justify-center">
-        <div className="flex gap-6 flex-wrap items-center justify-center">
-          <a
-            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-            href="https://github.com/Crossmint/headless-checkout-quickstart"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/file.svg"
-              alt="File icon"
-              width={16}
-              height={16}
-            />
-            View code
-          </a>
-          <a
-            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-            href="https://github.com/crossmint"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/window.svg"
-              alt="Window icon"
-              width={16}
-              height={16}
-            />
-            See all quickstarts
-          </a>
-          <a
-            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-            href="https://crossmint.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/globe.svg"
-              alt="Globe icon"
-              width={16}
-              height={16}
-            />
-            Go to crossmint.com →
-          </a>
-        </div>
-        <div className="flex">
-          <Image
-            src="/crossmint-leaf.svg"
-            alt="Powered by Crossmint"
-            priority
-            width={152}
-            height={100}
-          />
-        </div>
-      </footer>
-
       <CheckoutDialog
         selectedWeapon={selectedWeaponData}
         isOpen={isCheckoutOpen}
         onClose={() => setIsCheckoutOpen(false)}
       />
+      <Footer />
     </div>
   );
 }
